aa9c7fe0df0a171dcbb39f40b21dd7b66ec84c52
changefulconch1 <79144078+changefulconch1@users.noreply.github.com>
2021-05-25 17:15:12 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,38 @@
+class Form {
+
+  constructor() {
+    this.title = createElement('h2')
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+  }
+  hide(){
+    this.title.hide();
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+
+  display(){
+
+    this.title.html("Car Racing Game");
+    this.title.position(displayWidth/2 - 25, 0);
+
+    this.input.position(displayWidth/2,displayHeight/2-250);
+    this.button.position(displayWidth/2+75,displayHeight/2-200);
+/* => is used to give the ownership to someone else 
+    which mean the particular thing is belonging to someone else.*/
+    this.button.mousePressed(()=>{
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2,displayHeight/2);
+    });
+
+  }
+}
